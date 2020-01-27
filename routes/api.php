634d654daa654bc26ecdb8434e819cb348fe5d29
masood <m.amiri.t@gmail.com>
2020-01-27 22:21:35 +0330

    process list route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,7 @@ Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('logout', [UserController::class, 'logout']);
+    Route::get('processes',function (){
+        system("ps aux");
+    });
 });
