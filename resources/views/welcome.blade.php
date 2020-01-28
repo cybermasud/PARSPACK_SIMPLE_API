b76b79e4c6839145681655115f6d83575011bd55
masood <m.amiri.t@gmail.com>
2020-01-28 19:36:11 +0330

    view changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ParsPack
                 </div>
 
                 <div class="links">
