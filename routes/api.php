ea99a556b3bbb8fab0e8af6dec42bd86f41fc5f8
masood <m.amiri.t@gmail.com>
2020-01-29 00:25:53 +0330

    removed profile

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('user', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('login', [UserController::class, 'login']);
 Route::post('register', [UserController::class, 'register']);
 
